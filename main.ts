68fc3340e2f9b1c6de6b6e1bc219971cd91aae17
EarthenSkyMobile <24978329+EarthenSky@users.noreply.github.com>
2026-01-29 06:48:14 +0000

    compute qkv and apply rope to qk

--- main.ts
+++ main.ts
@@ -73,7 +73,7 @@ interface Config {
     hidden_dim: number;
     n_layers: number;
     n_heads: number;
-    m_kv_heads: number;
+    n_kv_heads: number;
     vocab_size: number;
     seq_len: number;
 }
@@ -86,8 +86,8 @@ interface Weights {
     rms_ffn: Float32Array;
 
     // multi-head attention
-    query: Float32Array;
-    key: Float32Array;
+    query: Float32Array; // (layer, dim, n_heads * head_size)
+    key: Float32Array;   // (layer, dim, n_kv_heads * head_size)
     value: Float32Array;
     output: Float32Array;
 
@@ -113,16 +113,17 @@ interface RuntimeBuffers {
     hb2: Float32Array;
 
     // for attention
-    q: Float32Array;
-    k: Float32Array;
-    v: Float32Array;
+    q: Float32Array; // (dim,)
+    // TODO: these are just pointers and need not be here
+    // k: Float32Array; // (not dim,)
+    // v: Float32Array; // (not dim,)
 
     // TODO: this
     att: Float32Array;
     logits: Float32Array;
 
-    // kv cache // TODO: ?
-    key_cache: Float32Array;
+    // kv cache
+    key_cache: Float32Array; // (
     value_cache: Float32Array;
 };
 
@@ -134,34 +135,93 @@ class Transformer {
         readonly b: RuntimeBuffers
     ) {}
 
+    static load_weights(readonly config: Config): Weights {
+        let head_size = config.dim / config.n_heads;
+        let weights: Weights = {
+
+             query: new Float32Array(),
+             key: new Float32Array(),
+             value: new Float32Array(config.dim * config.n_kv_heads * config.head_size),
+             output: new Float32Array(),
+        };  
+  
+        // TODO: load data from the checkpoint file here
+
+        return weights; 
+    }
+
     static create_buffers(readonly config: Config): RuntimeBuffers {
         // TODO: this
-        return RuntimeBuffers {
+        const head_size = config.dim / config.n_heads;
+        const kv_dim = config.n_kv_heads * head_size;
+        return {
+            x: new Float32Array(config.dim),
+            x_residual: new Float32Arrat(config.dim),
+             
+            q: new Float32Array(config.dim),
 
+            key_cache: new Float32Array(
+                config.n_layers * config.seq_len * kv_dim),
+            val_cache: new Float32Array(
+                config.n_layers * config.seq_len * kv_dim),
         };
     }
 
     forward(token: number, position: number) {
-        const dim = config->dim;       
+        const dim = config.dim;       
         // TODO: finish this + understanding the kv cache logic. Read into MQA, briefly
-        const kv_dim = (dim * config->n_kv_heads) / config->n_heads;
+        const head_size = dim / config.n_heads;
+        const kv_dim =  config.n_kv_heads * head_size;
         // TODO: get x (the embedding) from the
         // token embedding table
         let x = null;
 
         for (let layer_i = 0; layer_i < this.config.n_layers; layer_i++) {
             // TODO: implement this!
-            // this.forward_layer();
             rmsnorm(
-                b->x_residual,
+                b.x_residual,
                 x,
-                weights->rms_att + layer_i * dim
+                weights.rms_att + layer_i * dim
             );
            
             // key and value point to the kv cache
-            const loff = l * config->seq_len * kv_dim; // kv cache layer offset for convenience
-            errs->k = s->key_cache + loff + pos * kv_dim;
-            s->v = s->value_cache + loff + pos * kv_dim; 
+            const layer_off = layer_i * config.seq_len * kv_dim;
+            const key   = b->key_cache.subarray(layer_off + position * kv_dim, kv_dim);
+            const value = b->value_cache.subarray(layer_off + position * kv_dim, kv_dim);
+             
+            vecmatmul(
+                b.q,
+                weights.query + layer_i * dim * dim
+                b.x_residual);
+            vecmatmul(
+                key,
+                weights.key   + layer_i * dim*kv_dim
+                b.x_residual);
+            vecmatmul(
+                value,
+                weights.value + layer_i * dim * kv_dim
+                b.x_residual);
+
+            // RoPE relative positional encoding: complex-valued rotate q and k in each head
+            for (let i = 0; i < dim; i += 2) {
+                let head_dim = i % head_size;
+                let freq = 1.0f / Math.pow(10000.0f, head_dim / (float)head_size);
+                let val = pos * freq;
+
+                // how many vectors? 2 = q & k, 1 = q only
+                let rotn = (i < kv_dim ? 2 : 1);
+                for (let v = 0; v < rotn; v++) {
+                    // the vector to rotate (query or key)
+                    let vec = (v == 0 ? query : key);
+                    let v0 = vec[i];
+                    let v1 = vec[i+1];
+                    // repr each two elems as the mag of a rotated vector
+                    vec[i]   = v0 * Math.cos(val) - v1 * Math.sin(val);
+                    vec[i+1] = v0 * Math.sin(val) + v1 * Math.cos(val);
+                }
+            }
+
+
         }
     }
 
